3abf0d6f391de8e7c9bec0ff7c053f9f4adf2846
Alex Kostyniuk <kostyniuk7@gmail.com>
2019-09-28 17:31:03 +0300

    it works

--- systemProgramming/3-lab.js
+++ systemProgramming/3-lab.js
@@ -1,6 +1,12 @@
 'use strict';
 
 const expression = process.argv[2];
+const regex = /[_a-zA-Z][_a-zA-Z0-9]*/;
+const cyrillicPattern = /[\u0400-\u04FF]/;
+if (expression.match(cyrillicPattern)) {
+  console.log('Something went wrong');
+  process.exit(1);
+}
 
 const reserved = ['if', 'else', 'switch', 'case', 'default', 'break', 'int',
   'float', 'char', 'double', 'long', 'for', 'while', 'do', 'void', 'goto',
@@ -21,7 +27,7 @@ const getSymbols = (str, arrOfSymbols) => {
       if (temp.length > 1) symbolsUsed.push(arrOfSymbols[i]);
     }
   }
-  return symbolsUsed;
+  return Array.from(new Set(symbolsUsed));
 };
 
 const getReserved = (str, arrOfReserved) => {
@@ -33,20 +39,34 @@ const getReserved = (str, arrOfReserved) => {
   return reservedUsed;
 };
 
+const identiers = [];
+const getIdintiers = expression => {
+  const str = expression;
+  const current = str.match(regex);
+  if (current) {
+    identiers.push(current[0]);
+    return getIdintiers(str.replace(current, ''));
+  }
+  return Array.from(new Set(identiers));
+};
+
 const getNumbers = (str) => str.match(/\d+/g).map(Number);
 
-const symbolsUsed = new Set(getSymbols(expression, symbols));
+const symbolsUsed = getSymbols(expression, symbols);
 const reservedUsed = getReserved(expression, reserved);
 const numbersUsed = getNumbers(expression);
 
 console.log(reservedUsed);
 console.log(symbolsUsed);
+console.log(getIdintiers(expression));
 console.log(numbersUsed);
 
-const cyrillicPattern = /^[\u0400-\u04FF]+$/;
-
-const identifiers = /^[_a-zA-Z][_a-zA-Z0-9]+/g/;
-console.log(expression.match(identifiers));
-const variables = [];
-
-
+for (let i = 0; i < identiers.length; i++) {
+  for (let j = 0; j < reservedUsed.length; j++) {
+    if (identiers[i] === reservedUsed[j]) {
+      identiers.splice(i, 1);
+      i--;
+    }
+  }
+}
+console.log(identiers);
