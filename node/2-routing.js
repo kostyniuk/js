b6834801396f7173228f670c60a3d23479942289
kostyniuk <kostyniuk7@gmail.com>
2019-10-28 09:18:34 +0200

    basic http server with routing

---
+++ node/2-routing.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const http = require('http');
+
+const info = {
+  'name' : 'Alex',
+  'age' : 19,
+  'city' : 'Kyiv' 
+}
+
+const routing = {
+  '/': '<h1>Welcole to my server!!! </h1>',
+  '/me': info,
+  '/me/age': () => info.age,
+  '/me/name': info.name,
+  '/api/method1': (req, res) => {
+    console.log(req.url);
+    return { status: res.statusCode };
+  },
+  '/api/method2': req => ({
+    info,
+    url: req.url,
+    cookies: req.headers.cookie
+  }),
+};
+
+const types = {
+  object: JSON.stringify,
+  string: s => s,
+  number: n => n.toString(),
+  undefined: () => 'not found',
+  function: (fn, req, res) => JSON.stringify(fn(req, res)),
+};
+
+http.createServer((req, res) => {
+  const data = routing[req.url];
+  const type = typeof data;
+  const serializer = types[type];
+  const result = serializer(data, req, res);
+  res.end(result);
+}).listen(8000);
