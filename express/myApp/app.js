28591689b7215bfbd962d52efb752073700e1e77
kostyniuk <kostyniuk7@gmail.com>
2019-11-12 09:13:08 +0200

    middlewares should be used before route to root path!!!

--- express/myApp/app.js
+++ express/myApp/app.js
@@ -2,14 +2,34 @@
 
 const express = require('express');
 const app = express();
+const path = require('path');
 
 const PORT = 3000;
 
+app.use('/static', express.static(path.join(__dirname, 'public')));
+
+const logger = (req, res, next) => {
+  console.log('Logger printed something');
+  next();
+};
+
+
+const requestTime = function(req, res, next) {
+  req.requestTime = Date.now();
+  next();
+};
+
+app.use(logger);
+
+app.use(requestTime);
+
 app.get('/', (req, res) => {
-  res.send('Hello World!!!');
+  let responseText = 'Hello World!<br>';
+  responseText += '<small>Requested at: ' + req.requestTime + '</small>';
+  res.send(responseText);
 });
 
 app.listen(PORT, (err) => {
   if (err) return console.error(err);
-  console.log(`Example app is listening at ${PORT} port`);
+  console.log(`Example app is listening at http://localhost:${PORT} `);
 });
