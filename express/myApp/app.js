51a9894c9b40d74a03732d7d189604f912e8b962
kostyniuk <kostyniuk7@gmail.com>
2019-11-12 09:17:54 +0200

    issue with the same pathes and methods

--- express/myApp/app.js
+++ express/myApp/app.js
@@ -29,6 +29,19 @@ app.get('/', (req, res) => {
   res.send(responseText);
 });
 
+app.get('/user/:id', (req, res, next) => {
+  console.log('ID:', req.params.id);
+  next();
+}, (req, res) => {
+  res.send('User Info');
+});
+
+// handler for the /user/:id path, which prints the user ID
+// won't be ever get called
+app.get('/user/:id', (req, res) => {
+  res.end(req.params.id);
+});
+
 app.listen(PORT, (err) => {
   if (err) return console.error(err);
   console.log(`Example app is listening at http://localhost:${PORT} `);
