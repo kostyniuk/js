3d267d78d15e983a78f11925a55f8ca3bdbcd6d3
kostyniuk <kostyniuk7@gmail.com>
2020-06-13 20:18:51 +0300

    add static to get all hobbies

--- useful/3-reduce.js
+++ useful/3-reduce.js
@@ -1,42 +1,5 @@
 'use strict';
 
-class User {
-  constructor(info) {
-    this.id = info.id;
-    this.username = info.username;
-    this.age = info.age;
-    this.hobbies = info.hobbies;
-  }
-
-  get yearOfBirth() {
-    return new Date().getUTCFullYear() - this.age;
-  }
-}
-
-const arr = [
-  new User({
-    id: 1,
-    username: 'kostyniuk',
-    age: 20,
-    hobbies: ['NBA', 'football', 'music'],
-  }),
-  new User({
-    id: 2,
-    username: 'dloading',
-    age: 23,
-    hobbies: ['NBA', 'dogs', 'food'],
-  }),
-  new User({
-    id: 3,
-    username: 'isco',
-    age: 27,
-    hobbies: ['NBA', 'golf'],
-  }),
-];
-
-const calcSumOfPropeperty = (arr, property, initial = 0) =>
-  arr.reduce((prev, current) => prev + current[property], initial);
-
 const fetchPropertyFromArr = (arr, property) => {
   return arr.reduce((prev, current) => {
     if (Array.isArray(current[property])) {
@@ -46,8 +9,54 @@ const fetchPropertyFromArr = (arr, property) => {
     }
   }, []);
 };
+
+class User {
+  constructor(info) {
+    this.id = info.id;
+    this.username = info.username;
+    this.age = info.age;
+    this.hobbies = info.hobbies;
+  }
+
+  static allHobies(...params) {
+    return fetchPropertyFromArr(params, 'hobbies');
+  }
+  get yearOfBirth() {
+    return new Date().getUTCFullYear() - this.age;
+  }
+}
+
+const kostyniuk = new User({
+  id: 1,
+  username: 'kostyniuk',
+  age: 20,
+  hobbies: ['NBA', 'football', 'music'],
+});
+
+const dloading = new User({
+  id: 2,
+  username: 'dloading',
+  age: 23,
+  hobbies: ['NBA', 'dogs', 'food'],
+});
+
+const isco = new User({
+  id: 3,
+  username: 'isco',
+  age: 27,
+  hobbies: ['NBA', 'golf'],
+});
+
+const allHobbies = User.allHobies(kostyniuk, dloading, isco);
+console.log({ allHobbies });
+
+const arr = [kostyniuk, dloading, isco];
+
+const calcSumOfPropeperty = (arr, property, initial = 0) =>
+  arr.reduce((prev, current) => prev + current[property], initial);
+
 const totalAge = calcSumOfPropeperty(arr, 'age');
 const allUsernames = fetchPropertyFromArr(arr, 'username');
-const allHobbies = fetchPropertyFromArr(arr, 'hobbies');
+// const allHobbies = fetchPropertyFromArr(arr, 'hobbies');
 const allYearsOfBirth = fetchPropertyFromArr(arr, 'yearOfBirth');
-console.log({ totalAge, allUsernames, allHobbies, allYearsOfBirth });
+// console.log({ totalAge, allUsernames, allHobbies, allYearsOfBirth });
