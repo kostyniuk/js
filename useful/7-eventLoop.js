c3bc70c4356c241a575557591f17b6b352cc562a
kostyniuk <kostyniuk7@gmail.com>
2020-08-24 16:23:56 +0300

    playing around tiwth event loop

---
+++ useful/7-eventLoop.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const fs = require('fs');
+
+console.log('Start');
+
+setTimeout(() => {
+  console.log('Timeout 1, 0ms');
+}, 0);
+
+process.nextTick(() => {
+  console.log('Before everything else');
+
+  setTimeout(() => {
+    console.log('Timeout 2, 0ms');
+  }, 0);
+
+  setImmediate(() => {
+    console.log('Immediate 1');
+  });
+});
+
+fs.readFile(__filename, () => {
+  setTimeout(() => {
+    console.log('Timeout 3, 0ms');
+  }, 0);
+
+  setImmediate(() => {
+    console.log('Immediate 2');
+  });
+});
+
+setTimeout(() => {
+  console.log('Timeout 4, 20ms');
+}, 20);
+
+console.log('End');
